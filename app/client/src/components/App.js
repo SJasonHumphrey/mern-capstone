81d2bf191be1efc626ce078d4e745f7db5c88f74
Gisselle <g.roldan527@gmail.com>
2021-07-20 09:16:43 -0700

    centered

--- app/client/src/components/App.js
+++ app/client/src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import TypingTest from './TypingTest'
 import Resources from './Resources'
 
 function App() {
@@ -7,8 +6,9 @@ function App() {
   return (
     <>
     {/* landing page - explain app here and provide link to login register */}
-    <TypingTest/>
-    <Resources/>
+      <Resources/>
+
+      
     </>
   );
 }
--- app/client/src/components/Resources.js
+++ app/client/src/components/Resources.js
@@ -5,28 +5,38 @@ import Accordion from 'react-bootstrap/Accordion'
 export default function Resources() {
   return (
     <>
-        <Accordion>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>Javascript</Accordion.Header>
-                <Accordion.Body>
-                    <ul>
-                        <li><a href=" https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/">Free Code Camp</a></li> 
-                        <li><a href="https://javascript.info/">Javascript Info</a></li> 
-                        <li><a href="https://www.reddit.com/r/learnjavascript/">r/learnjavascript on Reddit</a></li> 
-                        <li><a href="https://zerotocode.today/">Zero To Code: How To Launch a Career in Software</a></li> 
-                    </ul>
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>React.js</Accordion.Header>
-                <Accordion.Body>
-                    <ul>
-                        <li><a href="https://reactjs.org/docs/getting-started.html">React.js Docs</a></li> 
-                        <li><a href="https://egghead.io/courses/the-beginner-s-guide-to-react">The Beginner's Guide to React</a></li> 
-                    </ul>
-                </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
+        <div className="container mt-5 mb-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4 className="col-md-6 offset-md-3">Useful Resources</h4>   
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Javascript</Accordion.Header>
+                            <Accordion.Body >
+                                <ul>
+                                    <li><a href=" https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/">Free Code Camp</a></li> 
+                                    <li><a href="https://javascript.info/">Javascript Info</a></li> 
+                                    <li><a href="https://www.reddit.com/r/learnjavascript/">r/learnjavascript on Reddit</a></li> 
+                                    <li><a href="https://zerotocode.today/">Zero To Code: How To Launch a Career in Software</a></li> 
+                                </ul>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>React.js</Accordion.Header>
+                            <Accordion.Body>
+                                <ul>
+                                    <li><a href="https://reactjs.org/docs/getting-started.html">React.js Docs</a></li> 
+                                    <li><a href="https://egghead.io/courses/the-beginner-s-guide-to-react">The Beginner's Guide to React</a></li> 
+                                </ul>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                            
+
+                </div>
+            </div>
+        </div>
+        
     </>
   )
 }
