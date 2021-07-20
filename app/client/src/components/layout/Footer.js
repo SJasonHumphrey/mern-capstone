b1279e6fcd4b3f44b8b009ace0a7181e3567c2bf
EC Carter <eca21988@gmail.com>
2021-07-20 10:39:04 -0400

    monday night updates

--- app/client/src/components/layout/Footer.js
+++ app/client/src/components/layout/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
                     <p className="d-flex justify-content-center align-items center pt-2">Copyright Team Async 2021</p>
                 </Col>
                 <Col sm={12} md={6}>
-                <p className="d-flex justify-content-center align-items center pt-2">some other copy here</p>
+                <p className="d-flex justify-content-center align-items center pt-2">github links here</p>
                 </Col>
             </Row>
         </Container>
--- app/client/src/components/layout/Header.js
+++ app/client/src/components/layout/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
     </Grid.Column> */}
         <Navbar collapseOnSelect expand="lg" className="footer-layout">
             <Navbar.Brand className="logo-top">
-            <Nav.Link as={Link} to="/">Team Async</Nav.Link>
+            <Nav.Link as={Link} to="/">Coding Buddy</Nav.Link>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="main-navbar"className="me-3"/>
             <Navbar.Collapse id="main-navbar">
