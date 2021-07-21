2d8a3b77467f2bd0e0b34ba90eccd0fbaa84b92d
Gisselle <g.roldan527@gmail.com>
2021-07-21 09:51:12 -0700

    added more links

--- app/client/src/components/Resources.js
+++ app/client/src/components/Resources.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Accordion } from 'react-bootstrap'
 import Accordion from 'react-bootstrap/Accordion'
 
 export default function Resources() {
@@ -7,10 +6,10 @@ export default function Resources() {
     <>
         <div className="container mt-5 mb-5">
             <div className="row">
-                <div className="col-md-6 offset-md-3">
-                    <h4 className="col-md-6 offset-md-3">Useful Resources</h4>   
+                <div className="col-md-10 offset-md-1">
+                    <h4 className="col-md-8 offset-md-4">Useful Resources</h4>   
                     <Accordion>
-                        <Accordion.Item eventKey="0">
+                        <Accordion.Item defaultActiveKey eventKey="0">
                             <Accordion.Header>Javascript</Accordion.Header>
                             <Accordion.Body >
                                 <ul>
@@ -18,6 +17,7 @@ export default function Resources() {
                                     <li><a href="https://javascript.info/">Javascript Info</a></li> 
                                     <li><a href="https://www.reddit.com/r/learnjavascript/">r/learnjavascript on Reddit</a></li> 
                                     <li><a href="https://zerotocode.today/">Zero To Code: How To Launch a Career in Software</a></li> 
+                                    <li><a href="https://zerotomastery.io/cheatsheets/javascript-cheatsheet-the-advanced-concepts/#call-stack-memory-heap">JavaScript Cheat Sheet: The Advanced Concepts</a></li> 
                                 </ul>
                             </Accordion.Body>
                         </Accordion.Item>
@@ -27,6 +27,24 @@ export default function Resources() {
                                 <ul>
                                     <li><a href="https://reactjs.org/docs/getting-started.html">React.js Docs</a></li> 
                                     <li><a href="https://egghead.io/courses/the-beginner-s-guide-to-react">The Beginner's Guide to React</a></li> 
+                                </ul>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>HTML & CSS</Accordion.Header>
+                            <Accordion.Body>
+                                <ul>
+                                    <li ><a href="https://sharkcoder.com/layout/flexbox">CSS Flexbox: A Complete Guide</a></li> 
+                                    <li><a href="https://bootstrap-cheatsheet.themeselection.com">Bootstrap 5 Cheat Sheet</a></li> 
+                                </ul>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>Miscellaneous</Accordion.Header>
+                            <Accordion.Body>
+                                <ul>
+                                    <li><a href="https://readme.so">Easiest Way to Create A README</a></li> 
+                                    <li><a href="https://www.markdownguide.org/cheat-sheet/">Markdown Cheat Sheet</a></li> 
                                 </ul>
                             </Accordion.Body>
                         </Accordion.Item>
