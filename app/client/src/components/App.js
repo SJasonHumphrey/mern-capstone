1aa795d8d7f2742b5dc267d0329893a29b83f4d2
EC Carter <eca21988@gmail.com>
2021-07-21 10:11:04 -0400

    tuesday night updates

--- app/client/src/components/App.js
+++ app/client/src/components/App.js
@@ -7,27 +7,22 @@ function App() {
   return (
     <Container fluid className="">
     <Row className="">
-      <Col sm={12} md={8}>
-        <div className="d-flex flex-column justify-content-center align-items-center full-height ">
-          <img src="images/test.png"  className="rem7" height=""/>
-          <h3>Worlds Best Coding Study Tool</h3>
-      <p className="text-center">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <img src="images/test.png"  className="rem7" height=""/>
-      </div>
+      <Col sm={12} md={6}>
+        <div className="d-flex justify-content-center align-items-center full-height">
+        <img src="images/color/study1.png" className="study-image" />
+        </div>
       </Col>
+
       <Col>
-      <div className="d-flex flex-column justify-content-center align-items-center  full-height">
-      <Button size="lg" variant="outline-dark" className="rounded-0">Make your study time count</Button>
-      </div>
+        <div className="d-flex flex-column justify-content-center align-items-center full-height ">
+        <p className="text-center p-5">
+        <h3 className="mono-font p-3">Worlds Best Coding Study Tool</h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Button size="lg" variant="outline-dark" className="rounded-0 mb-5 mono-font">Make your study time count</Button>
+        </div>
       </Col>
-      
-    
     </Row>
-   
-   
-    
     </Container>
   );
 }
--- app/client/src/components/layout/Header.js
+++ app/client/src/components/layout/Header.js
@@ -12,26 +12,23 @@ const Header = () => {
         <>
     
         <Navbar collapseOnSelect expand="lg" className="footer-layout">
-            <Navbar.Brand className="logo-top">
-            <Nav.Link as={Link} to="/"><img src="images/logo2.png" className="rem5"/></Nav.Link>
+            <Navbar.Brand className="">
+            <Nav.Link as={Link} to="/" className=""><img src="images/color/codingBuddyLogo.png" className="codingBuddyLogo"/></Nav.Link>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="main-navbar"className="me-3"/>
             <Navbar.Collapse id="main-navbar">
                 <Nav className=" ms-auto">
                     <Nav.Item >
-                    <Nav.Link as={Link} to="/login" >Login</Nav.Link>
+                    <Nav.Link as={Link} to="/login" className="mono-font">Login</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                    <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+                    <Nav.Link as={Link} to='/register' className="mono-font">Register</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                    <Nav.Link as={Link} to='/home'>Dashboard</Nav.Link>
+                    <Nav.Link as={Link} to='/home' className="mono-font">Dashboard</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                    <Nav.Link  onClick={handleShow}>Study Tools</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                    <Nav.Link as={Link} to=''></Nav.Link>
+                    <Nav.Link  onClick={handleShow} className="mono-font me-5">Study Tools</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
@@ -39,22 +36,22 @@ const Header = () => {
 
         <Offcanvas show={visible} onHide={handleClose} placement={'top'} >
             <Offcanvas.Header closeButton className="offCanvasColor">
-                <Offcanvas.Title>Study Tools</Offcanvas.Title>
+                <Offcanvas.Title className="mono-font ">Study Tools</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body className="offCanvasColor">
                 <Navbar>
                 <Nav>
                     <Nav.Item>
-                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w "><img src="images/color/keyboard.png" height="50px"/>Typing Practice</Nav.Link>
+                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w mono-font"><img src="images/color/keyboard.png" height="50px"/>Typing Practice</Nav.Link>
                     </Nav.Item>
                     <Nav.Item >
-                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w"><img src="images/color/laptopcoding.png" height="50px"/>Code Pen</Nav.Link>
+                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w mono-font"><img src="images/color/laptopcoding.png" height="50px"/>Code Pen</Nav.Link>
                     </Nav.Item>
                     <Nav.Item >
-                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w"><img src="images/color/language.png" height="50px"/>Flash Cards</Nav.Link>
+                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w mono-font"><img src="images/color/language.png" height="50px"/>Flash Cards</Nav.Link>
                     </Nav.Item>
                     <Nav.Item >
-                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w"><img src="images/color/writing-code.png" height="50px"/>Quizzes</Nav.Link>
+                    <Nav.Link as={Link} to="/login" onClick={handleClose} className="d-flex flex-column justify-content-center align-items-center rem3 rem12-w mono-font"><img src="images/color/writing-code.png" height="50px"/>Quizzes</Nav.Link>
                     </Nav.Item>
                     
 
