a0f09923ce9893711b73b6ebcf2a0131d6ffc107
Victoria Green <victoria.cousan@gmail.com>
2021-07-23 14:21:09 -0500

    connected front end to db

--- app/server/db.js
+++ app/server/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const env = require('./config/env');
 
-mongoose.connect(env.CONNECTION_URL, { useNewUrlParser: true,useUnifiedTopology: true } ,
-err => {
-    if(!err)
-    console.log('db connecion successful')
-    else
-    console.log('db connection error: '. JSON.stringify(err, undefined, 2))
-})
+
+const connectDB = async () => {
+    try {
+await mongoose.connect(env.CONNECTION_URL, { useNewUrlParser: true,useUnifiedTopology: true })
+console.log('db connection successful');
+    } catch(err){
+        console.log('db connection error'+err)
+    }
+}
+
+connectDB();
